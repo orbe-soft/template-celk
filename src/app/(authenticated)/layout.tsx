0712e9e9f1ebfdfcac4d13fc7660a2d5b51c01ea
Luan Souza <luan@orbesoft.com.br>
2024-03-26 21:39:14 -0300

    refactor: route group names

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -1,6 +1,6 @@
 import { PropsWithChildren } from "react";
 
-export default function DashboardLayout({ children }: PropsWithChildren) {
+export default function Layout({ children }: PropsWithChildren) {
   return (
     <main className="px-32 py-20 bg-[#F0F0F0] min-h-screen">{children}</main>
   );
